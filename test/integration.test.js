5fbb561f55ac23d48bb2b6547ea32152bf24d0cf
Dale Anderson <danderson00@gmail.com>
2019-02-03 22:58:59 +1100

    Added integration tests

--- test/integration.test.js
+++ test/integration.test.js
@@ -1,14 +1,43 @@
-const hostModule = require('../host')
-const consumerModule = require('../consumer')
+const host = require('../host')
+const consumer = require('../consumer')
 const WebSocket = require('ws')
 
+let server
+
+const setup = async api => {
+  server = new WebSocket.Server({ port: 1234 })
+  host(server, api)
+  return await consumer(new WebSocket('ws://localhost:1234'))
+}
+
+afterEach(() => server.close())
+
 test("simple API invocation", async () => {
-  const server = new WebSocket.Server({ port: 1234 })
-  const host = hostModule(server, { hello: () => 'world' }, { log: { level: 'trace' } })
-  const consumer = await consumerModule(new WebSocket('ws://localhost:1234'))
-
-  const result = await consumer.hello()
+  const api = await setup({ hello: () => 'world' })
+  const result = await api.hello()
   expect(result).toBe('world')
+})
 
-  server.close()
+test("asynchronous API", async () => {
+  const api = await setup({ hello: () => new Promise(res => setTimeout(() => res('world'), 10)) })
+  const result = await api.hello()
+  expect(result).toBe('world')
+})
+
+test("operation parameters", async () => {
+  const api = await setup({ hello: (a, d) => a.b + a.c + d })
+  const result = await api.hello({ b: 'wor', c: 1 }, 'd')
+  expect(result).toBe('wor1d')
+})
+
+test("errors result in rejected promises", async () => {
+  const api = await setup({ hello: () => { throw new Error('world') } })
+  const promise = api.hello()
+  await expect(promise).rejects.toEqual({ message: 'world' })
+})
+
+test("rejected promises are returned", async () => {
+  const api = await setup({ hello: () => Promise.reject(new Error('world'))})
+  const promise = api.hello()
+  await expect(promise).rejects.toEqual({ message: 'world' })
 })

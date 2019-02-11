7c03fc68c902bde00a32ef15dbc15ac74e63b9d7
Dale Anderson <danderson00@gmail.com>
2019-02-11 20:17:33 +1100

    Update observable response type

--- host/responseTypes/observable.js
+++ host/responseTypes/observable.js
@@ -2,19 +2,17 @@ const { isObservable, unwrap } = require('xest')
 
 module.exports = {
   test: isObservable,
-  handler: (value, { send }) => {
-    const subscription = value.subscribe(newValue => send.update({ value: unwrap(newValue) }))
-    send.ok({ type: 'observable', value: unwrap(value) }, 'persistent')
+  handler: (sessionObservable, resultObservable, send) => {
+    const resultSubscription = resultObservable.subscribe(newValue => send.update({ value: unwrap(newValue) }))
+    send.ok({ type: 'observable', value: unwrap(resultObservable) }, 'persistent')
 
-    return { 
-      messageHandler: ({ session }) => {
-        if(session === 'terminate') {
-          subscription.unsubscribe()
-          if(value.disconnect) {
-            value.disconnect()
-          }
+    sessionObservable.subscribe(({ session }) => {
+      if(session === 'terminate') {
+        resultSubscription.unsubscribe()
+        if(resultObservable.disconnect) {
+          resultObservable.disconnect()
         }
       }
-    }
+    })
   }
 }

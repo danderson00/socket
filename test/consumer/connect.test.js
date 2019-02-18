a7aae03dbefc7ce34f1dc119845cee24323f7d44
Dale Anderson <danderson00@gmail.com>
2019-02-18 20:28:55 +1100

    Add connect test

---
+++ test/consumer/connect.test.js
@@ -0,0 +1,13 @@
+const connect = require('../../consumer/connect')
+
+test("connect returns wrapped api", async () => {
+  const create = jest.fn().mockReturnValue(Promise.resolve(
+    { status: 'ok', data: { operations: [{ name: 'test' }] } }
+  ))
+  
+  const api = await connect({ create })
+  expect(api.test).toBeInstanceOf(Function)
+
+  api.test(1, 'test')
+  expect(create.mock.calls[1]).toEqual(['operation', { operation: 'test', parameters: [1, 'test'] }])
+})

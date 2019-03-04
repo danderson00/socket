7a0ddb1a309171acfcbf5064ad0d5270374af6ba
Dale Anderson <danderson00@gmail.com>
2019-03-04 18:47:34 +1100

    Add multiple consumers

--- test/observable.test.js
+++ test/observable.test.js
@@ -8,12 +8,11 @@ let server
 const setup = async api => {
   server = new WebSocket.Server({ port: 1234 })
   host(server, { log: { level: 'none' } }).useApi(api)
-  return await consumer({ socketFactory: () => new WebSocket('ws://localhost:1234') }).connect()
+  return await createConsumer()
 }
-
-afterEach(() => server.close())
-
 const delay = delay => new Promise(resolve => setTimeout(resolve, delay))
+const createConsumer = () => consumer({ socketFactory: () => new WebSocket('ws://localhost:1234') }).connect()
+afterEach(() => server.close())
 
 test("observable initial value", async () => {
   const api = await setup({ hello: () => subject({ initialValue: 'world' }) })
@@ -38,4 +37,16 @@ test("unsubscribe", async () => {
   source.publish('world')
   await delay(10)
   expect(result()).toBeUndefined()
+})
+
+test("multiple consumers", async () => {
+  const source = subject()
+  const api1 = await setup({ hello: () => source })
+  const result1 = await api1.hello()
+  const api2 = await createConsumer()
+  const result2 = await api2.hello()
+  source.publish('world')
+  await delay(10)
+  expect(result1()).toBe('world')
+  expect(result2()).toBe('world')
 })

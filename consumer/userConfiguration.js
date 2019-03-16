ca16b1021d8f99bf78fd1a0756d2c0a4d87008c2
Dale Anderson <danderson00@gmail.com>
2019-03-16 19:54:32 +1100

    Refactor

--- consumer/userConfiguration.js
+++ consumer/userConfiguration.js
@@ -6,6 +6,22 @@ const features = require('./features')
 module.exports = middleware => {
   const configurationChain = []
 
+  const configurationSeries = generator => configurationChain.reduce(
+    async (previous, x) => {
+      await previous
+      return generator(x)
+    },
+    Promise.resolve()
+  )
+
+  const builtInFeature = (name, options) => {
+    const builtIn = features[name]
+    if(!builtIn) {
+      throw new Error(`No such built-in feature ${name}`)
+    }
+    return builtIn(options)
+  }
+
   return {
     use: middleware => configurationChain.push({ middleware }),
     useFeature: (feature, options) => {
@@ -14,43 +30,23 @@ module.exports = middleware => {
       }
       configurationChain.push({ feature })
     },
-    initialize: context => (
-      configurationChain.reduce(
-        async (previous, x) => {
-          await previous
-          if(x.middleware) {
-            middleware.add(x.middleware)
-          }
-          if(x.feature) {
-            x.constructedFeature = await Promise.resolve(x.feature(context))
-            if(x.constructedFeature.middleware) {
-              middleware.add(x.constructedFeature.middleware)            
-            }
-          }
-        },
-        Promise.resolve()
-      )
-    ),
+    initialize: context => configurationSeries(async x => {
+      if(x.middleware) {
+        middleware.add(x.middleware)
+      }
+      if(x.feature) {
+        x.constructedFeature = await x.feature(context)
+        if(x.constructedFeature.middleware) {
+          middleware.add(x.constructedFeature.middleware)            
+        }
+      }
+    }),
     // middleware connect functions are not executed on initial connect
     // as they have not been constructed yet. rename -> reconnect?
-    connect: context => (
-      configurationChain.reduce(
-        async (previous, x) => {
-          await previous
-          if(x.constructedFeature && x.constructedFeature.connect) {
-            await Promise.resolve(x.constructedFeature.connect(context))
-          }
-        },
-        Promise.resolve()
-      )
-    )
+    connect: context => configurationSeries(async x => {
+      if(x.constructedFeature && x.constructedFeature.connect) {
+        await x.constructedFeature.connect(context)
+      }
+    })
   }
-}
-
-const builtInFeature = (name, options) => {
-  const builtIn = features[name]
-  if(!builtIn) {
-    throw new Error(`No such built-in feature ${name}`)
-  }
-  return builtIn(options)
 }

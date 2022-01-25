7b191287ee9b8116c4531beca7ec8e118b5e7662
Dale Anderson <danderson00@gmail.com>
2022-01-25 13:52:42 +1100

    Update sample

--- samples/share-location/public/index.js
+++ samples/share-location/public/index.js
@@ -1,27 +1,43 @@
 function onMapLoaded() {
-  navigator.geolocation.getCurrentPosition(async ({ coords }) => {
-    const initialLocation = { lat: coords.latitude, lng: coords.longitude }
-    const map = new google.maps.Map(document.getElementById("map"), { center: initialLocation, zoom: 15 })
-    const markers = {}
+  // get an initial position and center a new map with it
+  navigator.geolocation.getCurrentPosition(
+    async ({ coords }) => {
+      const initialLocation = { lat: coords.latitude, lng: coords.longitude }
+      const map = new google.maps.Map(document.getElementById("map"), { center: initialLocation, zoom: 15 })
+      const markers = {}
 
-    const host = await window.xsocket({ url: `ws://${window.location.hostname}:8081/` }) // use wss:// for hosted
-      .useFeature('reestablishSessions')
-      .connect()
+      const host = await window.xsocket({ url: `ws://${window.location.hostname}:8081/` })
+        .useFeature('reestablishSessions')
+        .useFeature('clientId')
+        .connect()
 
-    const locationStream = await host.locationStream()
-    locationStream.subscribe(updateMarker)
-    updateLocation(initialLocation)
+      const locationStream = await host.locationStream()
+      locationStream.subscribe(updateMarker)
+      updateLocation(initialLocation)
 
-    function updateLocation(location) {
-      host.registerLocation(location)
-      navigator.geolocation.getCurrentPosition(
-        ({ coords: { latitude: lat, longitude: lng } }) => updateLocation({ lat, lng })
-      )
-    }
+      // a recursive function to continually update our position
+      function updateLocation(location) {
+        host.registerLocation(location)
+        navigator.geolocation.getCurrentPosition(
+          ({ coords: { latitude: lat, longitude: lng } }) => {
+            updateLocation({ lat, lng })
+          },
+          reportError
+        )
+      }
 
-    function updateMarker({ lat, lng, address }) {
-      const marker = markers[address] = markers[address] || new google.maps.Marker({ title: address, map })
-      marker.setPosition({ lat, lng })
-    }
-  })
+      // update the marker for the specific clientId when updates are received
+      function updateMarker({ lat, lng, address, clientId }) {
+        const title = `Client ID: ${clientId}\nAddress: ${address}`
+        const marker = markers[clientId] = markers[clientId] || new google.maps.Marker({ title, map })
+        marker.setPosition({ lat, lng })
+      }
+    },
+    reportError
+  )
+
+  function reportError(error) {
+    console.error('An error occurred obtaining location: ', error)
+  }
 }
+
--- samples/share-location/src/index.js
+++ samples/share-location/src/index.js
@@ -1,22 +1,34 @@
-const index = require('@x/socket')
+const host = require('@x/socket')
 const { subject } = require('@x/observable')
 const Websocket = require('ws')
 
-const locations = subject()
+// an observable that encapsulates the stream of location updates
+const locationStream = subject()
+
+// a simple API to obtain the locationStream and publish a location update
 const api = {
-  locationStream: () => locations,
-  registerLocation: ({ lat, lng, address }) => locations.publish({ lat, lng, address })
+  locationStream: () => locationStream,
+  registerLocation: ({ lat, lng, address, clientId }) => (
+    locationStream.publish({ lat, lng, address, clientId })
+  )
 }
+
+// middleware to inject specific connection properties into the payload object
 const middleware = {
   registerLocation: ({ connection, next }, location) => {
-    next({ ...location, address: connection.socket._socket.remoteAddress })
+    return next({
+      ...location,
+      clientId: connection.clientId,
+      address: connection.socket._socket.remoteAddress
+    })
   }
 }
 
-// this will only work from localhost due to page being hosted with SSL to enable geolocation
-// to host on a remote server, create a https.Server here and configure with a certificate that is not self signed
-const options = {
-  server: new Websocket.Server({ port: 8081 })
-}
-index(options).useApi(api).use(middleware)
+// this will only work from localhost without SSL - accessing geolocation requires an encrypted connection.
+// to host on a remote server, create an https.Server here and configure with a certificate that is not self signed
+host({ server: new Websocket.Server({ port: 8081 }) })
+  // enable the clientId feature to uniquely identify consumer devices
+  .useFeature('clientId', { cipherKey: 'ENCRYPTION_KEY' })
+  .use(middleware)
+  .useApi(api)
 

7081ac5223fb28e67756961b700ac65b826e4cf8
Dale Anderson <danderson00@gmail.com>
2022-06-20 16:36:15 +1000

    Add react helper

---
+++ react/HostProvider.js
@@ -0,0 +1,44 @@
+import { createElement, createContext, useEffect, useState, useContext } from 'react'
+import { isObservable } from '@x/expressions'
+
+const instanceContext = createContext({})
+
+export const HostProvider = ({ consumer, component, loading = 'Loading...' }) => {
+  const [host, setHost] = useState()
+
+  useEffect(() => {
+    consumer.connect().then(host => setHost(host))
+  }, [consumer])
+
+  return createElement(instanceContext.Provider, {
+    value: { host },
+    children: host ? createElement(component, { host }) : loading
+  })
+}
+
+export const useHost = work => {
+  const { host } = useContext(instanceContext)
+  const [value, setValue] = useState()
+
+  useEffect(() => {
+    const promise = Promise.resolve(work(host))
+    promise.then(result => {
+      if(isObservable(result)) {
+        setValue(result())
+        result.subscribe(setValue)
+      } else {
+        setValue(result)
+      }
+    })
+
+    return () => {
+      promise.then(result => {
+        if(isObservable(result)) {
+          result.disconnect()
+        }
+      })
+    }
+  }, [host])
+
+  return value
+}

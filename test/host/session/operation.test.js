053cb5b2ba1b7ca71202f215599eead9ac2d67b4
Dale Anderson <danderson00@gmail.com>
2022-06-25 14:47:42 +1000

    Fix test

--- test/host/session/operation.test.js
+++ test/host/session/operation.test.js
@@ -64,7 +64,7 @@ test("operation returns error if API function does not exist", async () => {
     sessionId: 1,
     status: 'error',
     session: 'terminate',
-    data: new TypeError("Cannot read properties of undefined (reading 'apply')")
+    data: new TypeError("Cannot read property 'apply' of undefined")
   }]])
   expect(source.disconnect.mock.calls.length).toBe(1)
 })
